cc84d7d19f39e7a5f4d294bb32409306446e623e
yeabuyte <942803504@qq.com>
2018-05-30 09:17:20 +0800

    common.h 提交

--- Collect_all/Collect_all.cpp
+++ Collect_all/Collect_all.cpp
@@ -3,10 +3,34 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include "../common/common.h"
+
+class test
+{
+public:
+	test()
+	{
+
+	}
+	~test()
+	{
+
+	}
+	DISALLOW_EVIL_CONSTRUCTORS(test);
+protected:
+private:
+};
+
 
 int main()
 {
 	std::cout << "Hello World"<< std::endl;
+
+	//test t;
+	//test a(t);
+
+
+	getchar();
     return 0;
 }
 
--- common/common.h
+++ common/common.h
@@ -1,3 +1,61 @@
-/************************************************************************/
-/* commond.h                                                            */
-/************************************************************************/
+/*******************************************************************
+*@filename:     common.h
+*@author:       zxj
+*@date:         2018/5/29 19:44
+********************************************************************/
+
+#ifndef _COMMON_H
+#define _COMMON_H
+
+
+#include <string>
+#include <functional>
+
+#ifndef NDEBUG
+#include <assert.h>
+#endif
+
+#ifdef WIN32
+#pragma warning(disable:4819)
+#pragma warning(disable:4996)
+#define snprintf _snprintf
+
+#if _MSC_VER < 1500 // MSVC 2008
+#define vsnprintf _vsnprintf
+#endif
+#endif
+
+namespace baseCollect
+{
+	using std::string;
+	using namespace std::placeholders;
+
+
+	// disallow copy ctor and assign opt
+	#undef DISALLOW_EVIL_CONSTRUCTORS
+	#define DISALLOW_EVIL_CONSTRUCTORS(TypeName)    \
+    TypeName(const TypeName&) =delete;                         \
+    void operator=(const TypeName&) = delete;
+
+	// delete object safe
+	#define SAFE_DELETE(p)        \
+    if (NULL != p) {          \
+        delete p;             \
+        p = NULL;             \
+    }
+
+	// delete object safe
+	#define SAFE_DELETE_ARRAY(p)        \
+    if (NULL != p) {          \
+        delete []p;             \
+        p = NULL;             \
+    }
+
+	#define MIN(a,b) ((a)<(b)) ? (a) : (b)
+	#define MAX(a,b) ((a)>(b)) ? (a) : (b)
+
+}
+
+
+
+#endif
